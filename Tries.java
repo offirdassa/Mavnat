a45f72fc7bd67128f4fdef8c3198cfdd911a3a2d
offirdassa <80092279+offirdassa@users.noreply.github.com>
2021-05-03 17:34:57 +0300

    Update Tries.java

--- Tries.java
+++ Tries.java
@@ -2,7 +2,21 @@
 public class Tries {
     public static void main(String[] args) {
         AVLTree tree = new AVLTree();
-        tree.insert(2,false);
-        System.out.println(tree.min());
+        tree.insert(15, false);
+        tree.insert(22,true);
+        tree.insert(10,true);
+        tree.insert(24,true);
+        tree.insert(20,true);
+        tree.insert(4,true);
+        tree.insert(11,true);
+        tree.insert(18,true);
+        tree.insert(12,true);
+        tree.insert(2,true);
+        tree.insert(7,true);
+        tree.insert(1,true);
+        tree.insert(6,true);
+        tree.insert(8,true);
+        System.out.println(tree.insert(5,true));
+        System.out.println('a');
     }
 }
